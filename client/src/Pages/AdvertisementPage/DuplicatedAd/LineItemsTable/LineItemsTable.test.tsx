1e72dc1b1dc02f20d54fc77ea6bf1aec9dd16276
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-08-12 17:14:18 +0300

    FE - test for lineItemTable

--- client/src/Pages/AdvertisementPage/DuplicatedAd/LineItemsTable/LineItemsTable.test.tsx
+++ client/src/Pages/AdvertisementPage/DuplicatedAd/LineItemsTable/LineItemsTable.test.tsx
@@ -1,13 +1,17 @@
-import { screen, render } from "@testing-library/react";
+import { screen, render, waitFor } from "@testing-library/react";
 import { mockLineItems } from "../../../../mocks/lineItems";
 import { LineItemsTable } from "./LineItemsTable";
 import userEvent from "@testing-library/user-event";
+import axios from "axios";
+
+jest.mock("axios");
 
 describe("LineItemsTable", () => {
   it("Should render without crashing", () => {
     const mockProps = { lineItems: mockLineItems, rowLineItemSelection: jest.fn(), onLineItemRow: jest.fn() };
     render(<LineItemsTable {...mockProps} />);
   });
+
   it("Should show column headers", async () => {
     const mockProps = { lineItems: mockLineItems, rowLineItemSelection: jest.fn(), onLineItemRow: jest.fn() };
     render(<LineItemsTable {...mockProps} />);
@@ -23,15 +27,33 @@ describe("LineItemsTable", () => {
     expect(titleHeader).toBeInTheDocument();
     expect(statusHeader).toBeInTheDocument();
   });
+
   it("Should show line items in table", async () => {
     const mockProps = { lineItems: mockLineItems, rowLineItemSelection: jest.fn(), onLineItemRow: jest.fn() };
     const { container } = render(<LineItemsTable {...mockProps} />);
 
     const rows = container.querySelectorAll(".ant-table-tbody  tr");
-1
+
     expect(rows).toHaveLength(5);
   });
-  it("Should call axios on input type", async () => {});
+
+  it("Should call axios on input type", async () => {
+    const mockAxiosResponse = {
+      data: [mockLineItems[0]],
+    };
+
+    (axios.get as jest.Mock).mockReturnValue(mockAxiosResponse);
+
+    const mockProps = { lineItems: mockLineItems, rowLineItemSelection: jest.fn(), onLineItemRow: jest.fn() };
+    render(<LineItemsTable {...mockProps} />);
+
+    const inputSearch = screen.getByPlaceholderText("Search for Line items");
+    await userEvent.type(inputSearch, "cat");
+
+    await waitFor(() => {
+      expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/advertisement/lineItems?search=cat");
+    });
+  });
   it("Should render default table", () => {});
   it("Should filter line items by input search", () => {});
   it("Should checked some line items by default", () => {});
