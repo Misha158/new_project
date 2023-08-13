189da909147fccdfdff4fe6ea61ea01ace665902
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-08-13 15:10:06 +0300

    FE - test for modal

---
+++ client/src/Pages/AdvertisementPage/DuplicatedAd/Modal/Modal.test.tsx
@@ -0,0 +1,8 @@
+describe("test", () => {
+  it("Should open modal - and should close modal", () => {
+    // use rerender from render
+  });
+  it("Should call on next handler", () => {});
+  it("Should call on back handler", () => {});
+  it("Should submit form", () => {});
+});
