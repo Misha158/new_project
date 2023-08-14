02e3a962d6513c42fbee67590c1adde94040fde5
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-08-14 14:27:11 +0300

    FE - add test ReviewAdNames

--- client/src/Pages/AdvertisementPage/DuplicatedAd/ReviewAdNames/ReviewAdNames.test.tsx
+++ client/src/Pages/AdvertisementPage/DuplicatedAd/ReviewAdNames/ReviewAdNames.test.tsx
@@ -1,3 +1,22 @@
+import { screen, render } from "@testing-library/react";
+import { ReviewAdNames } from "./ReviewAdNames";
+import { mockAds } from "../../../../mocks/ads";
+import { mockLineItems } from "../../../../mocks/lineItems";
+
+const mockProps = {
+  selectedLineItemsRows: [mockLineItems[0]],
+  selectedAd: mockAds[0],
+  setAdNameLineItems: () => {},
+  adNameLineItems: {},
+};
+
 describe("<ReviewAdNames />", () => {
-  it("test", () => {});
+  it("Should render without crashing", () => {
+    render(<ReviewAdNames {...mockProps} />);
+  });
+  it("Should render line item inputs with auto incremented names", () => {
+    render(<ReviewAdNames {...mockProps} />);
+  });
+  it("Should render line item inputs with auto incremented names and edited names", () => {});
+  it("Should change ad name correctly", () => {});
 });
