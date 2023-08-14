76a775d44e222595872ea62bf47784399b0a4ae0
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-08-14 14:20:48 +0300

    FE - add test adInput

--- client/src/Pages/AdvertisementPage/DuplicatedAd/ReviewAdNames/AdInput/AdInput.test.tsx
+++ client/src/Pages/AdvertisementPage/DuplicatedAd/ReviewAdNames/AdInput/AdInput.test.tsx
@@ -1,10 +1,15 @@
-import { render } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import { AdInput } from "./AdInput";
 import { mockAds } from "../../../../../mocks/ads";
 import { mockLineItems } from "../../../../../mocks/lineItems";
+import userEvent from "@testing-library/user-event";
 
 const mockProps = {
-  adNameLineItems: [],
+  adNameLineItems: {
+    "lineItemId-4": {
+      editedAdName: "editedAdName",
+    },
+  },
   setAdNameLineItems: () => {},
   lineItem: mockLineItems[0],
   selectedAd: mockAds[0],
@@ -18,6 +23,19 @@ describe("<AdInput />", () => {
 
   it("Should return edited ad name", () => {
     render(<AdInput {...mockProps} />);
+
+    expect(screen.getByDisplayValue(/editedadname/i)).toBeInTheDocument();
   });
-  it("Should set new ad name correctly", () => {});
+
+  it("Should set new ad name correctly", async () => {
+    const mockSetAdNameLineItems = jest.fn();
+
+    render(<AdInput {...{ ...mockProps, adNameLineItems: {}, setAdNameLineItems: mockSetAdNameLineItems }} />);
+    const inputAdName = screen.getByRole("textbox");
+
+    await userEvent.type(inputAdName, "new-ad-name");
+
+    expect(mockSetAdNameLineItems).toHaveBeenCalled();
+    expect(screen.getByDisplayValue(/new-ad-name/i)).toBeInTheDocument();
+  });
 });
