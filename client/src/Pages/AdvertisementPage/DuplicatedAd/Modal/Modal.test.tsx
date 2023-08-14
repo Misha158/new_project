ac6d62e2d0ead7a19c36321ab77822bafbe72322
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-08-14 11:36:32 +0300

    FE - Modal tests, using mock hooks

--- client/src/Pages/AdvertisementPage/DuplicatedAd/Modal/Modal.test.tsx
+++ client/src/Pages/AdvertisementPage/DuplicatedAd/Modal/Modal.test.tsx
@@ -1,8 +1,72 @@
-describe("test", () => {
+import { screen, render } from "@testing-library/react";
+import { Modal } from "./Modal";
+import { mockLineItems } from "../../../../mocks/lineItems";
+import { mockAds } from "../../../../mocks/ads";
+import userEvent from "@testing-library/user-event";
+
+jest.mock("../../hooks/useSelectedRows", () => ({
+  useSelectedRows: () => ({
+    selectedRows: [mockLineItems[0]],
+    onRow: () => {},
+    rowSelection: [],
+  }),
+}));
+
+jest.mock("../hooks/useSetAdNameLineItems", () => ({
+  useSetAdNameLineItems: () => ({
+    adNameLineItems: {},
+    setAdNameLineItems: () => {},
+  }),
+}));
+
+describe("<Modal />", () => {
   it("Should open modal - and should close modal", () => {
+    const mockProps = {
+      step: 1,
+      isModalOpen: false,
+      lineItems: mockLineItems,
+      selectedAdRows: [mockAds[0]],
+      closeModal: () => {},
+      handleNext: () => {},
+      handleBack: () => {},
+    };
+
     // use rerender from render
+    const { rerender } = render(<Modal {...mockProps} />);
+
+    expect(screen.queryByText("Basic Modal")).not.toBeInTheDocument();
+
+    rerender(<Modal {...{ ...mockProps, isModalOpen: true }} />);
+    screen.logTestingPlaygroundURL();
+    expect(screen.getByText("Basic Modal")).toBeInTheDocument();
   });
-  it("Should call on next handler", () => {});
-  it("Should call on back handler", () => {});
-  it("Should submit form", () => {});
+
+  it("Should call on next and on back handlers", async () => {
+    const mockOnNext = jest.fn();
+    const mockOnBack = jest.fn();
+
+    const mockProps = {
+      step: 1,
+      isModalOpen: true,
+      lineItems: mockLineItems,
+      selectedAdRows: [mockAds[0]],
+      closeModal: () => {},
+      handleNext: mockOnNext,
+      handleBack: mockOnBack,
+    };
+
+    // use rerender from render
+    const { rerender } = render(<Modal {...mockProps} />);
+    const nextBtn = screen.getByText(/next/i);
+
+    await userEvent.click(nextBtn);
+    expect(mockOnNext).toHaveBeenCalledTimes(1);
+
+    rerender(<Modal {...{ ...mockProps, step: 2 }} />);
+
+    const backBtn = screen.getByText(/back/i);
+
+    await userEvent.click(backBtn);
+    expect(mockOnBack).toHaveBeenCalledTimes(1);
+  });
 });
---
+++ client/src/mocks/ads.ts
@@ -0,0 +1,44 @@
+export const mockAds = [
+  {
+    id: 4,
+    title: "Ad 4",
+    status: "Paused",
+    line_item_id: 4,
+    campaign_id: 3,
+  },
+  {
+    id: 7,
+    title: "Ad 7",
+    status: "Active",
+    line_item_id: 7,
+    campaign_id: 5,
+  },
+  {
+    id: 11,
+    title: "Ad 11",
+    status: "Paused",
+    line_item_id: 11,
+    campaign_id: 8,
+  },
+  {
+    id: 12,
+    title: "Ad 12",
+    status: "Active",
+    line_item_id: 12,
+    campaign_id: 9,
+  },
+  {
+    id: 13,
+    title: "Ad 13",
+    status: "Inactive2",
+    line_item_id: 13,
+    campaign_id: 9,
+  },
+  {
+    id: 15,
+    title: "Ad 1",
+    status: "Active",
+    line_item_id: 13,
+    campaign_id: 9,
+  },
+];
