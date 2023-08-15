4ce124b0bbedbba791ad5534cc253bf5d8bb2307
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-08-15 12:33:21 +0300

    FE - add test duplicatedAd

--- client/src/Pages/AdvertisementPage/DuplicatedAd/DuplicatedAd.test.tsx
+++ client/src/Pages/AdvertisementPage/DuplicatedAd/DuplicatedAd.test.tsx
@@ -1,3 +1,20 @@
+import { screen, render, waitFor } from "@testing-library/react";
+import { DuplicatedAd } from "./DuplicatedAd";
+import { mockLineItems } from "../../../mocks/lineItems";
+import { mockAds } from "../../../mocks/ads";
+import userEvent from "@testing-library/user-event";
+
 describe("<DuplicatedAd />", () => {
-  it("test", () => {});
+  it("Should render without crashing", () => {
+    render(<DuplicatedAd lineItems={mockLineItems} selectedAdRows={mockAds[0]} />);
+  });
+  it("Should show tooltip message", async () => {
+    render(<DuplicatedAd lineItems={mockLineItems} selectedAdRows={mockAds[0]} />);
+    const btn = screen.getByText(/duplicate ad/i);
+    await userEvent.hover(btn);
+    await waitFor(() => {
+      screen.logTestingPlaygroundURL();
+    });
+  });
+  it("Should call open modal on click at button", () => {});
 });
