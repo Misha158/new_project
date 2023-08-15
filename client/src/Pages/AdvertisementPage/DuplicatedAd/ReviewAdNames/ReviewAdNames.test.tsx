fb0d20c5e15c76077855f75ec3d6ecd811b179ac
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-08-15 10:49:50 +0300

    FE - add test ReviewAdNames

--- client/src/Pages/AdvertisementPage/DuplicatedAd/ReviewAdNames/ReviewAdNames.test.tsx
+++ client/src/Pages/AdvertisementPage/DuplicatedAd/ReviewAdNames/ReviewAdNames.test.tsx
@@ -2,9 +2,10 @@ import { screen, render } from "@testing-library/react";
 import { ReviewAdNames } from "./ReviewAdNames";
 import { mockAds } from "../../../../mocks/ads";
 import { mockLineItems } from "../../../../mocks/lineItems";
+import userEvent from "@testing-library/user-event";
 
 const mockProps = {
-  selectedLineItemsRows: [mockLineItems],
+  selectedLineItemsRows: mockLineItems,
   selectedAd: mockAds[0],
   setAdNameLineItems: () => {},
   adNameLineItems: {
@@ -18,10 +19,22 @@ describe("<ReviewAdNames />", () => {
   it("Should render without crashing", () => {
     render(<ReviewAdNames {...mockProps} />);
   });
-  it("Should render line item inputs with auto incremented names", () => {
+
+  it("Should render line item inputs with auto incremented names and edited names", () => {
     render(<ReviewAdNames {...mockProps} />);
-    screen.logTestingPlaygroundURL();
+
+    expect(screen.getByDisplayValue("editedAdName")).toBeInTheDocument();
+    expect(screen.getByDisplayValue(/ad 4_2/i)).toBeInTheDocument();
+    expect(screen.getByDisplayValue(/ad 4_3/i)).toBeInTheDocument();
+    expect(screen.getByDisplayValue(/ad 4_4/i)).toBeInTheDocument();
   });
-  it("Should render line item inputs with auto incremented names and edited names", () => {});
-  it("Should change ad name correctly", () => {});
+  it("Should change ad name correctly", async () => {
+    render(<ReviewAdNames {...mockProps} />);
+    const firstInput = screen.getByDisplayValue("editedAdName");
+
+    await userEvent.clear(firstInput);
+    await userEvent.type(firstInput, "new-changed-ad-name");
+
+    expect(screen.getByDisplayValue("new-changed-ad-name")).toBeInTheDocument();
+  });
 });
