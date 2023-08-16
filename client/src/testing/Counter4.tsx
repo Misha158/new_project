4929680fdd84cac724eb8a104ae65faa0a892358
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-08-16 12:56:26 +0300

    FE - ROLL BACK change place for sonarqube

---
+++ client/src/testing/Counter4.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { Button } from "antd";
+
+export const Counter4 = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const decrement = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  return (
+    <div>
+      <Button onClick={increment}>Increment</Button>
+      <Button onClick={decrement}>Decrement</Button>
+    </div>
+  );
+};
