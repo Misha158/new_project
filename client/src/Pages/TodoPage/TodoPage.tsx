a8672d8811488e1b3aa3c101f0d2050538dce411
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-08-16 13:18:32 +0300

    FE - Test sonarqube

--- client/src/Pages/TodoPage/TodoPage.tsx
+++ client/src/Pages/TodoPage/TodoPage.tsx
@@ -1,12 +1,21 @@
 import { Button, Input } from "antd";
 import { TodoList } from "../../shared/components/TodoList/TodoList";
 import { useAddTodo, useTodos } from "./hooks/useTodoPage";
+import { useState } from "react";
 
 export const TodoPage = () => {
   const { todos, setTodos } = useTodos();
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   const { newTodoText, isShowAddNewTodoInput, onChangeNewTodo, onShowInputOrSaveNewTodo } = useAddTodo({ setTodos });
+
+  // DELETE LATER TEST FOR SONARQUBE
+  const [test, setTest] = useState(false);
+
+  if (test) {
+    return <div>test</div>;
+  }
+  // DELETE LATER TEST FOR SONARQUBE
 
   return (
     <div>
