5f26975f21937ed5592404ae4fc08b6f682d769f
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-08-16 16:01:17 +0300

    Sonar - add ignore coverage

--- client/src/testing/Counter.test.tsx
+++ client/src/testing/Counter.test.tsx
@@ -1,8 +1,9 @@
-import { render } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import { Counter } from "./Counter";
 
 describe("Counter", () => {
   it("Should render without crashing", () => {
     render(<Counter />);
+    expect(screen.getByText("Decrement")).toBeInTheDocument();
   });
 });
