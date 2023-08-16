4df92dd264958c310d2ef47b6f9bb05b26668ead
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-08-16 14:53:36 +0300

    Sonar - Add one test

---
+++ client/src/Pages/TodoPage/TodoPage.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import { TodoPage } from "./TodoPage";
+
+describe("TodoPage", () => {
+  it("Should test test", () => {
+    render(<TodoPage />);
+  });
+});
