11e49adc0e84d33109d76c9d8552aeaa4acb9d67
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-08-20 23:30:01 +0300

    MB-1 FE - refactoring useEffects with requets

--- client/src/Pages/AdvertisementPage/hooks/useFetchTableData.ts
+++ client/src/Pages/AdvertisementPage/hooks/useFetchTableData.ts
@@ -37,62 +37,72 @@ interface Props {
   selectedLineItemIds: number[];
 }
 
+const fetchAds = async ({ selectedCampaignIds, selectedLineItemIds }) => {
+  const ads = await axios.get(`http://localhost:3000/advertisement/ads?campaignIds=[${selectedCampaignIds}]&lineItemIds=[${selectedLineItemIds}]`);
+
+  return ads;
+};
+
+const fetchLineItemsAndAds = async ({ selectedCampaignIds, selectedLineItemIds }) => {
+  const lineItems = await axios.get(`http://localhost:3000/advertisement/lineItems?campaignIds=[${selectedCampaignIds}]`);
+  const ads = await fetchAds({ selectedCampaignIds, selectedLineItemIds });
+
+  return { lineItems, ads };
+};
+
+const fetchAll = async () => {
+  const campaigns = await axios.get("http://localhost:3000/advertisement/campaigns");
+  const lineItems = await axios.get("http://localhost:3000/advertisement/lineItems");
+  const ads = await axios.get("http://localhost:3000/advertisement/ads");
+
+  return {
+    campaigns,
+    lineItems,
+    ads,
+  };
+};
+
 export const useFetchTableData = ({ selectedCampaignIds, selectedLineItemIds }: Props): Result => {
   const [campaigns, setCampaigns] = useState<Campaign[]>([]);
   const [lineItems, setLineItems] = useState<LineItem[]>([]);
   const [ads, setAds] = useState<Ad[]>([]);
 
+  const [oldSelectedCampaignIds, setOldSelectedCampaignIds] = useState(selectedCampaignIds);
+  const [oldSelectedLineItemIds, setOldSelectedLineItemIds] = useState(selectedLineItemIds);
+
   const ref = useRef(false);
 
   useEffect(() => {
-    const fetchDada = async () => {
-      const campaigns = await axios.get("http://localhost:3000/advertisement/campaigns");
-      const lineItems = await axios.get("http://localhost:3000/advertisement/lineItems");
-      const ads = await axios.get("http://localhost:3000/advertisement/ads");
-
-      setCampaigns(campaigns.data);
-      setLineItems(lineItems.data);
-      setAds(ads.data);
-    };
-
-    fetchDada();
-  }, []);
-
-  useEffect(() => {
     if (!ref.current) {
+      const fetchAllData = async () => {
+        const { campaigns, lineItems, ads } = await fetchAll();
+
+        setCampaigns(campaigns.data);
+        setLineItems(lineItems.data);
+        setAds(ads.data);
+      };
+
+      void fetchAllData();
+
       ref.current = true;
       return;
     }
 
-    const fetchData = async () => {
-      const lineItems = await axios.get(`http://localhost:3000/advertisement/lineItems?campaignIds=[${selectedCampaignIds}]`);
-      const ads = await axios.get(
-        `http://localhost:3000/advertisement/ads?campaignIds=[${selectedCampaignIds}]&lineItemIds=[${selectedLineItemIds}]`
-      );
+    if (oldSelectedCampaignIds !== selectedCampaignIds) {
+      const { lineItems, ads } = fetchLineItemsAndAds({ selectedLineItemIds, selectedCampaignIds });
 
       setLineItems(lineItems.data);
       setAds(ads.data);
-    };
 
-    fetchData();
-  }, [selectedCampaignIds]);
-
-  useEffect(() => {
-    if (!ref.current) {
-      ref.current = true;
-      return;
-    }
-
-    const fetchData = async () => {
-      const ads = await axios.get(
-        `http://localhost:3000/advertisement/ads?campaignIds=[${selectedCampaignIds}]&lineItemIds=[${selectedLineItemIds}]`
-      );
+      setOldSelectedCampaignIds(selectedCampaignIds);
+    } else if (oldSelectedCampaignIds === selectedCampaignIds && oldSelectedLineItemIds !== selectedLineItemIds) {
+      const ads = fetchAds({ selectedCampaignIds, selectedLineItemIds });
 
       setAds(ads.data);
-    };
 
-    fetchData();
-  }, [selectedLineItemIds]);
+      setOldSelectedLineItemIds(selectedLineItemIds);
+    }
+  }, [selectedCampaignIds, selectedLineItemIds]);
 
   return {
     campaigns,
