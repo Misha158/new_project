f0aadac85db821a5aec90744606d2d52cec95673
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-08-25 20:25:53 +0300

    MB-2 - FE - tests for DeleteAd

--- client/src/Pages/AdvertisementPage/DeleteAd/DeleteAd.test.tsx
+++ client/src/Pages/AdvertisementPage/DeleteAd/DeleteAd.test.tsx
@@ -1,3 +1,40 @@
-describe("test", () => {
-  it("test", () => {});
+import { screen, render, waitFor } from "@testing-library/react";
+import { DeleteAd } from "./DeleteAd";
+import { mockAds } from "../../../mocks/ads";
+import userEvent from "@testing-library/user-event";
+
+describe("<DeleteAd />", () => {
+  it("Button should be disabled, if selectedAdRows is empty", () => {
+    const mockProps = {
+      selectedAdRows: [],
+      setAds: jest.fn(),
+    };
+
+    render(<DeleteAd {...mockProps} />);
+
+    const btn = screen.getByRole("button", {
+      name: /delete ad/i,
+    });
+
+    expect(btn).toBeDisabled();
+  });
+
+  it("Should open modal with table with ads which will be deleted", async () => {
+    const mockProps = {
+      selectedAdRows: mockAds,
+      setAds: jest.fn(),
+    };
+
+    render(<DeleteAd {...mockProps} />);
+
+    const btn = screen.getByRole("button", {
+      name: /delete ad/i,
+    });
+
+    await userEvent.click(btn);
+
+    await waitFor(() => {
+      expect(window.document.querySelector(".ant-modal-content")).toBeInTheDocument();
+    });
+  });
 });
