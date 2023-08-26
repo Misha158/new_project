56ff2b630cb5f420e55d5a2300c39a14958b1bee
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-08-26 15:32:09 +0300

    MB-3 BE - started adding sequelize

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,9 +6,25 @@ import postRouter from "./routers/postRouter";
 import todoRouter from "./routers/todoRouter";
 import advertisementRouter from "./routers/advertisementRouter";
 import { dbConfig } from "./config";
+import { Sequelize } from "sequelize";
 
 const app = express();
 const port = 3000;
+
+const sequelize = new Sequelize({
+  dialect: "mysql",
+  username: "root",
+  password: "",
+  database: "misha",
+  host: "localhost",
+});
+
+try {
+  await sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
 
 // Создаем пул подключения к базе данных
 export const pool = mysql2.createPool(dbConfig);
