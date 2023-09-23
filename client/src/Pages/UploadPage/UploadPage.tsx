b6d9bac4bc74575776aa04b0e0265af2b93377dd
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-09-23 11:58:09 +0300

    Added droppable area

--- client/src/Pages/UploadPage/UploadPage.tsx
+++ client/src/Pages/UploadPage/UploadPage.tsx
@@ -6,11 +6,22 @@ import "./style.scss";
 // use react hook form
 export const UploadPage = () => {
   const [file, setFile] = useState(null);
+  const [isDragging, setIsDragging] = useState(false);
 
-  const fileInput = useRef<HTMLInputElement>(null);
+  const fileInput = useRef<HTMLInputElement | null>(null);
 
   const onUpload = () => {
     fileInput.current?.click();
+  };
+
+  const onDragStart = (e) => {
+    e.preventDefault();
+    setIsDragging(true);
+  };
+
+  const onDragLeave = (e) => {
+    e.preventDefault();
+    setIsDragging(false);
   };
 
   return (
@@ -21,6 +32,15 @@ export const UploadPage = () => {
           <Button onClick={onUpload}>upload</Button>
           <input type="file" ref={fileInput} className="uploader__input" />
         </label>
+
+        <div
+          className={`uploader__drag_area ${isDragging ? "active" : ""}`}
+          onDragStart={onDragStart}
+          onDragLeave={onDragLeave}
+          onDragOver={onDragStart}
+        >
+          <div>{isDragging ? "Leave file here" : "Drag files here"}</div>
+        </div>
 
         <Button>Save photo</Button>
       </form>
