71a5b2bd828d787f259b96c3079ad6024a74a2ed
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-09-23 10:37:15 +0300

    Fixed versions of node module packages

--- client/src/Pages/UploadPage/UploadPage.tsx
+++ client/src/Pages/UploadPage/UploadPage.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Button } from "antd";
 
+import "./style.scss";
+
+// use react hook form
 export const UploadPage = () => {
-  return <div>UploadPage</div>;
+  const [file, setFile] = useState(null);
+
+  const fileInput = useRef();
+
+  return (
+    <div>
+      <h1 className="main">UploadPage</h1>
+      <form>
+        <input type="file" ref={fileInput} />
+        <Button>Save photo</Button>
+      </form>
+    </div>
+  );
 };
