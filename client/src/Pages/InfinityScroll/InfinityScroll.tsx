c011cc6af489907a93408370d3c979f459ac1b78
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-10-09 23:45:49 +0300

    FE - Added infinity scroll with request to server

--- client/src/Pages/InfinityScroll/InfinityScroll.tsx
+++ client/src/Pages/InfinityScroll/InfinityScroll.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { axios } from "../../services/config";
 
 const style = {
   height: 30,
@@ -8,12 +9,38 @@ const style = {
   padding: 8,
 };
 
-export const InfinityScroll = () => {
-  const [items, setItems] = useState(Array.from({ length: 20 }));
+interface Posts {
+  id: number;
+  title: string;
+}
 
-  const fetchMoreData = () => {
-    setTimeout(() => {
-      setItems((prevItems) => prevItems.concat(Array.from({ length: 20 })));
+export const InfinityScroll = () => {
+  const [items, setItems] = useState<Posts[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("https://jsonplaceholder.typicode.com/posts?_limit=20");
+      console.log("result", result);
+
+      setItems(result.data);
+      setTotal(+result.headers["x-total-count"]);
+    };
+
+    fetchData();
+  }, []);
+
+  const fetchMoreData = async () => {
+    setTimeout(async () => {
+      const result = await axios.get("https://jsonplaceholder.typicode.com/posts?_limit=20", {
+        params: {
+          _page: currentPage + 1,
+        },
+      });
+
+      setItems((prev) => [...prev, ...result.data]);
+      setCurrentPage((prev) => prev + 1);
     }, 1500);
   };
 
@@ -21,10 +48,10 @@ export const InfinityScroll = () => {
     <div>
       <h1>demo: react-infinite-scroll-component</h1>
       <hr />
-      <InfiniteScroll dataLength={items.length} next={fetchMoreData} hasMore={true} loader={<h4>Loading...</h4>}>
-        {items.map((i, index) => (
+      <InfiniteScroll dataLength={items.length} next={fetchMoreData} hasMore={currentPage * 20 < total} loader={<h4>Loading...</h4>}>
+        {items.map((item, index) => (
           <div style={style} key={index}>
-            div - #{index}
+            id - #{item?.id}
           </div>
         ))}
       </InfiniteScroll>
