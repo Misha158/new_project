63fc1cf990aa58adb7aa31da605c36930fb6debd
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-10-11 11:17:46 +0300

    CS - recursion of tree

--- client/src/js/test.js
+++ client/src/js/test.js
@@ -18,23 +18,58 @@
 // console.log(firstDuplicateLetter("abcdefe")); // e
 // console.log(firstDuplicateLetter("abcde")); // null
 
-const closure = () => {
-  let count = 0;
+// const closure = () => {
+//   let count = 0;
+//
+//   const getCount = () => count;
+//
+//   const incrementCount = () => {
+//     count++;
+//   };
+//
+//   return {
+//     getCount,
+//     incrementCount,
+//   };
+// };
+//
+// const counter = closure();
+// console.log(counter.incrementCount());
+// console.log(counter.incrementCount());
+// console.log(counter.incrementCount());
+// console.log(counter.getCount());
 
-  const getCount = () => count;
-
-  const incrementCount = () => {
-    count++;
-  };
-
-  return {
-    getCount,
-    incrementCount,
-  };
+const tree = {
+  value: 1,
+  children: [
+    {
+      value: 2,
+      children: [
+        {
+          value: 3,
+          children: [],
+        },
+        {
+          value: 4,
+          children: [],
+        },
+      ],
+    },
+    {
+      value: 5,
+      children: [],
+    },
+  ],
 };
 
-const counter = closure();
-console.log(counter.incrementCount());
-console.log(counter.incrementCount());
-console.log(counter.incrementCount());
-console.log(counter.getCount());
+const traverseTree = (tree) => {
+  let result = tree.value;
+  for (const child of tree.children) {
+    // result = result + traverseTree(child);
+    result += traverseTree(child);
+  }
+
+  return result;
+};
+
+console.log(traverseTree(tree));
